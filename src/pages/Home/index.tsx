85e67c709e70013e0002abd62e8d1fe0ae11970e
Tatiana Rodríguez <rodriguezjessicatatiana@gmail.com>
2023-06-21 07:45:18 -0500

    feat: home page added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom"
 import './Home.scss'
 export function Home() {
+
   return(
-    <div>Aqui va el home</div>
+    <section className="home__container">
+      <div className="home__content-container">
+        <h1 className="home__content-title">Bienvenido al mejor buscador de peliculas</h1>
+        <p className="home__content-subtitle">Para buscar películas, por favor da click <Link to="/search">aquí</Link> </p>
+      </div>
+    </section>
   )
 }
