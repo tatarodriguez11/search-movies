8ae61d8cd4c08c5114da367d636533dff37c0ea7
Tatiana Rodríguez <rodriguezjessicatatiana@gmail.com>
2023-06-21 08:09:22 -0500

    feat: not found page added

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
 import './NotFound.scss'
-
-export function NotFound() {
+export function NotFound(){
   return(
-    <div>Aqui va el not found</div>
+    <div className="notFound__container">
+      <div className="notFound__content-container">
+        <h1 className="notFound__content-title">OOPS! pareces algo perdido</h1>
+        <p className="notFound__content-subtitle">Para seguir buscando pelis, entra <Link to="/search">aquí</Link></p>
+      </div>
+    </div>
   )
 }
